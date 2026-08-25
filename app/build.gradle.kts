3a10d7dea0649bf05326cd7e63c2ace8afcf864f
Sagi Weizmann <38720312+sagiweizmann@users.noreply.github.com>
2026-08-26 01:45:28 +0300

    Build Python runtime for ARM Android TV

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
         targetSdk = 35
         versionCode = 2
         versionName = "0.2.0"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a")
+        }
     }
 
     compileOptions {
